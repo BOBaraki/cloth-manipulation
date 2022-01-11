2d6491f62f926e524738e3800e7d7a2444eae09e
BOBaraki <bobosgithub@gmail.com>
2022-01-11 13:20:05 +0100

    extra files for labeling

--- label_dataset.py
+++ label_dataset.py
@@ -8,26 +8,55 @@ import csv
 
 import pdb
 
-PATH = "/media/gtzelepis/DATA/real_data/videos/data/two_hands_side/"
+PATH = "/media/gtzelepis/DATA/real_data/more_videos/videos/white_medium/two_hand_middle/"
 EXT = "*.csv"
 img_EXT = "*.png"
+fn = 'white_medium_two_hand_middle_'
 
-header = ['filename', 'cloth_state']
+header = ['filename', 'cloth_state', 'false_labeling']
 
-cloth_state = ['flat', 'semi-lifted-twohands', 'folded']
+# cloth_state = ['flat', 'semi-lifted-onehand', 'diagonally Folded']
+# cloth_state = ['flat', 'semi-lifted-twohands', 'folded']
+# cloth_state = ['flat', 'semi-lifted-onehand', 'folded']
+cloth_state = ['flat', 'semi-lifted-twohands-middle']
+
 
 flat = True
 semi = False
 folded = False
 
 
+# def stateidentification(filename, flat, semi, folded):
+#     if filename == fn+'100001' or filename == fn+'100119':
+#         flat = True
+#         semi = False
+#         folded = False
+#         # lifted = False
+#     elif name == fn+'100026' or name == fn+'200038':
+#         flat = False
+#         semi = True
+#         folded = False
+#         # lifted = False
+#     # elif name == 'one_hand_lifting_100089' or name == 'one_hand_lifting_200098':
+#     #     flat = False
+#     #     semi = False
+#     #     semi_crampled = True
+#     #     lifted = False
+#     elif name == fn+'100074' or name == fn+'200092':
+#         flat = False
+#         semi = False
+#         folded = True
+#         # lifted = True
+#     return flat, semi, folded
+
+
 def stateidentification(filename, flat, semi, folded):
-    if filename == 'two_hands_side_100227' or filename == 'two_hands_side_100001':
+    if filename == fn+'100001' or filename == fn+'100122' or filename == fn+'200124':
         flat = True
         semi = False
         folded = False
         # lifted = False
-    elif name == 'two_hands_side_100074' or name == 'two_hands_side_200072':
+    elif name == fn+'100035' or name == fn+'200042' or name == fn+'300054':
         flat = False
         semi = True
         folded = False
@@ -37,7 +66,7 @@ def stateidentification(filename, flat, semi, folded):
     #     semi = False
     #     semi_crampled = True
     #     lifted = False
-    elif name == 'two_hands_side_100142' or name == 'two_hands_side_200132':
+    elif name == fn+'10008700' or name == fn+'200090000' or name == fn+'3000910000':
         flat = False
         semi = False
         folded = True
