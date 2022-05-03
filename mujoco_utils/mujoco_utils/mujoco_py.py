39f7602d253e083149b59ecb698709dc99cccd19
Barbany <oriolbm96@gmail.com>
2022-05-03 18:44:06 +0200

    Return both intrinsic and extrinsic matrices

--- mujoco_utils/mujoco_utils/mujoco_py.py
+++ mujoco_utils/mujoco_utils/mujoco_py.py
@@ -3,8 +3,8 @@ from mujoco_py.cymj import PyMjvCamera
 from mujoco_utils.camera import get_camera_extrinsic_matrix, get_camera_intrinsic_matrix
 
 
-def get_camera_transform_matrix(width, height, vertical_fov, camera: PyMjvCamera):
-    """Get the camera matrix from a mujoco_py camera converting world coordinates to image coordinates.
+def get_camera_transform_matrices(width, height, vertical_fov, camera: PyMjvCamera):
+    """Get the camera matrices from a mujoco_py camera converting world coordinates to image coordinates.
 
     Args:
         width (int): Width of the rendered image.
@@ -13,17 +13,17 @@ def get_camera_transform_matrix(width, height, vertical_fov, camera: PyMjvCamera
         camera (PyMjvCamera): mujoco_py camera with azimuth, distance, elevation, and lookat attributes.
 
     Returns:
-        np.ndarray: 3x4 camera matrix.
+        Tuple[np.ndarray, np.ndarray]: Tuple of intrinsic (3x3) and extrinsic (3x4) cameras.
     """
+    intrinsic_matrix = get_camera_intrinsic_matrix(
+        width=width,
+        height=height,
+        vertical_fov=np.radians(vertical_fov),
+    )
     extrinsic_matrix = get_camera_extrinsic_matrix(
         azimuth=np.radians(camera.azimuth),
         distance=camera.distance,
         elevation=np.radians(camera.elevation),
         lookat=camera.lookat,
     )
-    intrinsic_matrix = get_camera_intrinsic_matrix(
-        width=width,
-        height=height,
-        vertical_fov=np.radians(vertical_fov),
-    )
-    return intrinsic_matrix @ extrinsic_matrix
+    return intrinsic_matrix, extrinsic_matrix
